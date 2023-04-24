9ffe0567890c4cb3644a896b9ef2f46695649b4a
Anushka Verma <anushkaverma779@gmail.com>
2023-04-25 04:01:41 +0530

    Project Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [userInfo, setUserInfo] = useState({});
+  const [error, setError] = useState();
+  const [flag, setFlag] = useState(false);
+
+async function handleSearch() {
+    setFlag(true);
+    axios.get(`https://api.github.com/users/${inputText}`)
+    .then((response) => {setUserInfo(response.data); setError(null);})
+    .catch((err) => {
+      setUserInfo(null);
+      setError("User Not Found");
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='search-container'>
+        <input type='text' className='search-input' placeholder='Search user...' onChange={(e) => {setInputText(e.target.value)}} value={inputText} />
+        <button className='search-btn' onClick={handleSearch}>Search</button>
+      </div>
+
+      {error && <p className='white error'>{error}</p>}
+      {
+        (inputText && flag && userInfo) && (
+          <div className='white githubUserInfo'>
+            <div className='top'>
+              <img src={userInfo.avatar_url} alt='Developer Photo' />
+              <h3>{userInfo.name}</h3>
+              <h5>{userInfo.login}</h5>
+              <p id='bio'>{userInfo.bio}</p>
+            </div>
+            <div className='bottom'>
+              <div className='left'>
+                <p>{"Repositories : " + userInfo.public_repos}</p>
+                <p>{"Followers : " + userInfo.followers}</p>
+                <p>{"Following : " + userInfo.following}</p>
+              </div>
+              <div className='right'>
+                <p>{"Location : " + ((userInfo.location) ? userInfo.location : "No Location")}</p>
+                <p>{"Company : " + ((userInfo.company) ? userInfo.company : "No Company")}</p>
+                <p>{"Twitter : @" + ((userInfo.twitter_username) ? userInfo.twitter_username : "No Twitter")}</p>
+              </div>
+            </div>
+          </div>
+        )
+      }
+    </>
   );
 }
 
